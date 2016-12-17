78136712558a2ab5ee13ab854fe2fc1cbda3f774
Robert Ng <robertng85@gmail.com>
2016-12-16 22:08:00 -0500

    css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <div id="container">
+        <div id="nav">
+          <h1> Robert_Ng </h1>
+          <h2> Manhattan, NY • 100% Match </h2>
+
+        </div>
+      </div>
+    );
+  }
+}
