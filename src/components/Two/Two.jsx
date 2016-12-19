59610ed9e22c0a58ed5843c82e37f8940c6f7ad8
Robert Ng <robertng85@gmail.com>
2016-12-19 01:12:28 -0500

    added percentages

--- src/components/Two/Two.jsx
+++ src/components/Two/Two.jsx
@@ -9,10 +9,16 @@ class Two extends Component {
 
     return (
       <div className="two-summary">
-      <h3>The two of us</h3>
+      <h3><span id="two">The two of us</span></h3>
+      <div className="horizontal">
         <img src={require('./htmlcss.png')} id="htmlcss" className="two-icons"/>
         <img src={require('./javascript.png')} id="javascript" className="two-icons"/>
         <img src={require('./react.png')} id="react" className="two-icons"/>
+      <div id="htmlcss-hundred">100%</div>
+      <div id="javascript-hundred">100%</div>
+      <div id="react-hundred">100%</div>
+      </div>
+
       </div>
     )
   };
